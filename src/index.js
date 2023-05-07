f9afd40776cb28b7ce16a1f2d390bfdf5cdd8f0a
Amal Nandan <amal.nandan@skizzle.tech>
2023-05-08 00:32:36 +0530

    Clam + Drop Roles with buttons

--- src/index.js
+++ src/index.js
@@ -10,39 +10,64 @@ const client = new Client({
 })
 client.on('ready', (c) => {
     console.log(`🤖 ${c.user.username} is 🟢 online`)
-
 })
-client.on('messageCreate', (message) => {
-    // console.log(message.content);
-    if (message.author.bot) {
-        return;
+client.on('interactionCreate', async (interaction) => {
+    try{
+     if(!interaction.isButton()) return;
+     await interaction.deferReply({
+         ephemeral:true,
+     });
+     const role = interaction.guild.roles.cache.get(interaction.customId);
+     if(!role){
+         interaction.editReply({
+             content:'I couldnt find the role',
+         })
+         return
+     }
+     const  hasRole = interaction.member.roles.cache.has(role.id);
+     if(hasRole){
+         await interaction.member.roles.remove(role);
+         await interaction.editReply(`The role ${role} has been removed.`);
+         return
+     }
+     await interaction.member.roles.add(role);
+     await interaction.editReply(`The role ${role} has been added.`);
+    } catch(error){
+     console.log(error);
+     
     }
-    if (message.content == 'hey') {
-        message.reply(`Hello ${message.author} what can i do for you? 😊`);
-    }
+ })
+// client.on('messageCreate', (message) => {
+//     // console.log(message.content);
+//     if (message.author.bot) {
+//         return;
+//     }
+//     if (message.content == 'hey') {
+//         message.reply(`Hello ${message.author} what can i do for you? 😊`);
+//     }
 
-});
-client.on('interactionCreate', (interaction) => {
-    if (!interaction.isChatInputCommand()) return;
-    if (interaction.commandName === 'embed') {
-        const embed = new EmbedBuilder()
-            .setTitle("Embed Title")
-            .setDescription('This is Embed Description')
-            .setColor('Random')
-            .addFields(
-                {
-                    name: 'Field title',
-                    value: 'Some Random value',
-                    inline: true,
-                },
-                {
-                    name: 'Field title 2',
-                    value: 'Some Random value',
-                    inline: true,
-                }
-            );
-        interaction.reply({ embeds: [embed] })
-    }
+// });
+// client.on('interactionCreate', (interaction) => {
+//     if (!interaction.isChatInputCommand()) return;
+    // if (interaction.commandName === 'embed') {
+    //     const embed = new EmbedBuilder()
+    //         .setTitle("Embed Title")
+    //         .setDescription('This is Embed Description')
+    //         .setColor('Random')
+    //         .addFields(
+    //             {
+    //                 name: 'Field title',
+    //                 value: 'Some Random value',
+    //                 inline: true,
+    //             },
+    //             {
+    //                 name: 'Field title 2',
+    //                 value: 'Some Random value',
+    //                 inline: true,
+    //             }
+    //         );
+    //     interaction.reply({ embeds: [embed] })
+    // }
     // console.log(interaction.commandName);
     // if(interaction.commandName === 'hey'){
     //     interaction.reply('hey!')
@@ -55,26 +80,26 @@ client.on('interactionCreate', (interaction) => {
     //     const num2 = interaction.options.get('second-number')?.value;
     //     interaction.reply(`The Sum is ${num1 + num2}`)
     // }
-})
-client.on('messageCreate', (message) => {
-    if (message.content === 'embed') {
-        const embed = new EmbedBuilder()
-        .setTitle("Embed Title")
-        .setDescription('This is Embed Description')
-        .setColor('Random')
-        .addFields(
-            {
-                name: 'Field title',
-                value: 'Some Random value',
-                inline: true,
-            },
-            {
-                name: 'Field title 2',
-                value: 'Some Random value',
-                inline: true,
-            }
-        );
-        message.channel.send({embeds: [embed]})
-    }
-})
+    // })
+    // client.on('messageCreate', (message) => {
+    //     if (message.content === 'embed') {
+    //         const embed = new EmbedBuilder()
+    //         .setTitle("Embed Title")
+    //         .setDescription('This is Embed Description')
+    //         .setColor('Random')
+    //         .addFields(
+    //             {
+    //                 name: 'Field title',
+    //                 value: 'Some Random value',
+    //                 inline: true,
+    //             },
+    //             {
+    //                 name: 'Field title 2',
+    //                 value: 'Some Random value',
+    //                 inline: true,
+    //             }
+    //         );
+    //         message.channel.send({embeds: [embed]})
+    //     }
+// })
 client.login(process.env.TOKEN);
