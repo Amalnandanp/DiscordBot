bd30d7b0dd14d1b54bffc5b0d6f22b65425fe80c
Amal Nandan <amal.nandan@skizzle.tech>
2023-05-07 17:32:26 +0530

    Slash command option and choices

--- src/index.js
+++ src/index.js
@@ -24,12 +24,17 @@ client.on('messageCreate' , (message) => {
 });
 client.on('interactionCreate', (interaction) => {
     if(!interaction.isChatInputCommand()) return;
-    console.log(interaction.commandName);
-    if(interaction.commandName === 'hey'){
-        interaction.reply('hey!')
-    }
-    if(interaction.commandName === 'ping'){
-        interaction.reply('pong!')
+    // console.log(interaction.commandName);
+    // if(interaction.commandName === 'hey'){
+    //     interaction.reply('hey!')
+    // }
+    // if(interaction.commandName === 'ping'){
+    //     interaction.reply('pong!')
+    // }
+    if(interaction.commandName === 'add'){
+        const num1 = interaction.options.get('first-number')?.value;
+        const num2 = interaction.options.get('second-number')?.value;
+        interaction.reply(`The Sum is ${num1 + num2}`)
     }
 })
 client.login(process.env.TOKEN);
